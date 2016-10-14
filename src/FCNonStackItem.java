37f880493682b11fe2ec2b6b159ba89730c37fb5
Anya_Smirnova <anya.sm1rnova@yandex.ru>
2016-10-14 17:31:13 +0700

    Update and rename FCBottle.java to FCNonStackItem.java

--- src/FCNonStackItem.java
+++ src/FCNonStackItem.java
@@ -2,10 +2,10 @@ package net.minecraft.src;
 
 import java.util.Random; 
 
-public class FCBottle extends Item 
+public class FCNonStackItem extends Item 
 { 
 
-        public FCBottle (int i) 
+        public FCNonStackItem (int i) 
         { 
                 super(i); 
                 maxStackSize = 1;
