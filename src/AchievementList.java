500058fd59f034b2df169be5e728938275509723
Anya_Smirnova <anya.sm1rnova@yandex.ru>
2016-10-18 07:51:57 +0700

    Update AchievementList.java

--- src/AchievementList.java
+++ src/AchievementList.java
@@ -126,11 +126,11 @@ public class AchievementList
         buildBetterPickaxe = (new Achievement(9, "buildBetterPickaxe", 6, 2, Item.pickaxeStone, buildPickaxe)).registerAchievement();
         cookFish = (new Achievement(10, "cookFish", 2, 6, Item.fishCooked, buildFurnace)).registerAchievement();
         onARail = (new Achievement(11, "onARail", 2, 3, Block.rail, acquireIron)).setSpecial().registerAchievement();
-        buildSword = (new Achievement(12, "buildSword", 6, -1, Item.swordSteel, buildWorkBench)).registerAchievement();
-        killEnemy = (new Achievement(13, "killEnemy", 8, -1, Item.bone, buildSword)).registerAchievement();
-        killCow = (new Achievement(14, "killCow", 7, -3, Item.leather, buildSword)).registerAchievement();
+        buildSword = (new Achievement(12, "buildSword", 0, 3, Item.swordSteel, acquireIron)).registerAchievement();
+        killEnemy = (new Achievement(13, "killEnemy", -2, 3, Item.bone, buildSword)).registerAchievement();
+        killCow = (new Achievement(14, "killCow", 0, 1, Item.leather, buildSword)).registerAchievement();
         flyPig = (new Achievement(15, "flyPig", 8, -4, Item.saddle, killCow)).setSpecial().registerAchievement();
-        snipeSkeleton = (new Achievement(16, "snipeSkeleton", 7, 0, Item.bow, killEnemy)).setSpecial().registerAchievement();
+        snipeSkeleton = (new Achievement(16, "snipeSkeleton", -4, 3, Item.bow, killEnemy)).setSpecial().registerAchievement();
         diamonds = (new Achievement(17, "diamonds", -1, 5, Item.diamond, acquireIron)).registerAchievement();
         portal = (new Achievement(18, "portal", -1, 7, Block.obsidian, diamonds)).registerAchievement();
         ghast = (new Achievement(19, "ghast", -4, 8, Item.ghastTear, portal)).setSpecial().registerAchievement();
@@ -139,7 +139,7 @@ public class AchievementList
         theEnd = (new Achievement(22, "theEnd", 3, 10, Item.eyeOfEnder, blazeRod)).setSpecial().registerAchievement();
         theEnd2 = (new Achievement(23, "theEnd2", 4, 13, Block.dragonEgg, theEnd)).setSpecial().registerAchievement();
         enchantments = (new Achievement(24, "enchantments", -4, 4, Block.enchantmentTable, diamonds)).registerAchievement();
-        overkill = (new Achievement(25, "overkill", -4, 1, Item.swordDiamond, enchantments)).setSpecial().registerAchievement();
+        overkill = (new Achievement(25, "overkill", -4, 1, Item.swordGold, enchantments)).setSpecial().registerAchievement();
         bookcase = (new Achievement(26, "bookcase", -3, 6, Block.bookShelf, enchantments)).registerAchievement();
         System.out.println((new StringBuilder()).append(achievementList.size()).append(" achievements").toString());
     }
